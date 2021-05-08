bac44157a208d89d640a7f0b7c7dfa79631dd425
Devin MacGillivray <devhmac@gmail.com>
2021-05-08 17:17:44 -0600

    makes pager conversion obj

--- react-front-end/src/helpers/pagerConversion.js
+++ react-front-end/src/helpers/pagerConversion.js
@@ -1,6 +1,20 @@
 const pagerConversion = {
-  green: {},
-  yellow: {},
-  orange: {},
-  red: {}
+  red: {
+    damage: "$1 Billion+",
+    fatalities: "1,000+"
+  },
+  orange: {
+    damage: "$100 Million - $1 Billion",
+    fatalities: "100 - 999"
+  },
+  yellow: {
+    damage: "$1 million - $100 million",
+    fatalities: "1 - 99"
+  },
+  green: {
+    damage: "Under $1 million",
+    fatalities: "0"
+  }
 };
+
+export default pagerConversion;
