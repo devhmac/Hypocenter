68363310176cb4b772259db2062aae343f85a0ea
Devin MacGillivray <devhmac@gmail.com>
2021-05-08 16:34:03 -0600

    Quake info list added

--- react-front-end/src/components/QuakeInfo.jsx
+++ react-front-end/src/components/QuakeInfo.jsx
@@ -1,11 +1,40 @@
 import React from "react";
-import { Button } from "@material-ui/core";
+import { List, ListItem, ListItemText } from "@material-ui/core";
 
 function QuakeInfo(props) {
   return (
-    <Button variant="contained" color="primary">
-      Earthquakes!
-    </Button>
+    <List>
+      <ListItem>
+        <ListItemText primary="Magnitude" secondary={props.magnitude} />
+      </ListItem>
+      <ListItem>
+        <ListItemText primary="Pager Status" secondary={props.pager} />
+      </ListItem>
+      <ListItem>
+        <ListItemText
+          primary="Estimated Economic Damage"
+          secondary={"Secondary Text"}
+        />
+      </ListItem>
+      <ListItem>
+        <ListItemText
+          primary="Potential Fatalities"
+          secondary={"Secondary Text"}
+        />
+      </ListItem>
+      <ListItem>
+        <ListItemText
+          primary="Time of Occurence"
+          secondary={props.time_stamp}
+        />
+      </ListItem>
+      <ListItem>
+        <ListItemText
+          primary="Danger of Tsunami"
+          secondary={props.tsunami ? "Yes" : "No"}
+        />
+      </ListItem>
+    </List>
   );
 }
 
