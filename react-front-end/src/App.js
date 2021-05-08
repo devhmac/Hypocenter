c0f798d9eb3a3d3472b2165d29e4deae384bf667
faroukalsajee <faroukalsajee@gmail.com>
2021-05-08 16:31:52 -0700

    refactor to jsx with material ui

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,50 +1,47 @@
-import React, { Component, useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { Component, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 import EqMap from "./components/EqMap.js";
-import QuakeInfo from './components/QuakeInfo'
-import NavBar from './components/NavBar'
-
+import QuakeInfo from "./components/QuakeInfo";
+import NavBar from "./components/NavBar";
+import CommentButton from "./components/Buttons/CommentButton.jsx";
+import DeleteButton from "./components/Buttons/DeleteButton.jsx";
 
 function App() {
-
   const [state, setState] = useState({
-    message: 'Click the button to load data!',
+    message: "Click the button to load data!",
     latitude: 0,
-    longitude: 0
+    longitude: 0,
   });
 
   const fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+    axios
+      .get("/api/data") // You can simply make your requests to "/api/whatever you want"
       .then((response) => {
         // handle success
-        console.log(response.data) // The entire response from the express server route
+        console.log(response.data); // The entire response from the express server route
 
-        console.log(response.data.message) // Just the message
+        console.log(response.data.message); // Just the message
         setState({
           title: response.data[0].title,
           latitude: response.data[0].latitude,
-          longitude: response.data[0].longitude
+          longitude: response.data[0].longitude,
         });
-      })
-  }
+      });
+  };
 
   return (
     <div className="App">
       <NavBar />
       <h1>{state.title}</h1>
-      <button onClick={fetchData} >
-        Fetch Data
-        </button>
-      <EqMap
-        latitude={state.latitude}
-        longitude={state.longitude}
-      />
+      <button onClick={fetchData}>Fetch Data</button>
+      <EqMap latitude={state.latitude} longitude={state.longitude} />
       <QuakeInfo />
+      <CommentButton />
+      <DeleteButton />
     </div>
   );
 }
-
 
 export default App;
