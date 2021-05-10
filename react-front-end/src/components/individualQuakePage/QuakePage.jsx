90c37fd2314ce0f32d0d7ccdee6d66d1d58041fe
Devin MacGillivray <devhmac@gmail.com>
2021-05-10 14:34:43 -0600

    adds buttons to quakePage, instead of rendering in App.js

--- react-front-end/src/components/individualQuakePage/QuakePage.jsx
+++ react-front-end/src/components/individualQuakePage/QuakePage.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { stateContext } from "../../contextProviders/stateContext";
 import QuakeInfo from "./QuakeInfo";
 import EqMap from "./EqMap";
+import CommentButton from "../Buttons/CommentButton";
+import DeleteButton from "../Buttons/DeleteButton";
 
 export default function QuakePage(props) {
   const { state, useState } = useContext(stateContext);
@@ -9,9 +11,11 @@ export default function QuakePage(props) {
 
   return (
     <div>
-      <h1>state.title</h1>
+      <h1>{state.title}</h1>
       <EqMap />
       <QuakeInfo />
+      <CommentButton />
+      <DeleteButton />
     </div>
   );
 }
