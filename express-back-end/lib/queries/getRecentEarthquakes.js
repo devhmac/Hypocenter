6a5dc65dc353f5b72f4623460e1194928bfb5826
Devin MacGillivray <devhmac@gmail.com>
2021-05-11 16:58:25 -0600

    adds query to get earthquakes added in the last 119 seconds

--- express-back-end/lib/queries/getRecentEarthquakes.js
+++ express-back-end/lib/queries/getRecentEarthquakes.js
@@ -2,15 +2,18 @@ const { response } = require('express');
 const db = require('../db');
 
 // test query
-const getEarthquakeData = (limit = 20) => {
-  return db.query('SELECT * FROM earthquakes LIMIT $1', [limit])
+const getRecentEarthquakes = (limit = 20) => {
+  return db.query(
+    `SELECT * FROM earthquakes
+    WHERE added BETWEEN NOW() - INTERVAL '119 SECONDS' AND NOW()`)
     .then((response) => {
+      console.log('recent eq', response.rows);
       return response.rows;
     });
 };
 
 module.exports = {
 
-  getEarthquakeData
+  getRecentEarthquakes
 
 };
