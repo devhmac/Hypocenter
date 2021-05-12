ad4a3fdf9f2afaccb752ca4d861911f6298408e8
Devin MacGillivray <devhmac@gmail.com>
2021-05-12 12:14:47 -0600

    adds universal button component

---
+++ react-front-end/src/components/Buttons/universalButton.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      marginTop: "20px",
+    },
+  },
+}));
+
+export default function UniversalButton(props) {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Button variant="contained" color="primary" onClick={props.onClick}>
+        {props.children}
+      </Button>
+    </div>
+  );
+}
