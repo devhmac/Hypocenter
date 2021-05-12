778af644d2e9e3c62697d18d7b96b5eca989484f
Devin MacGillivray <devhmac@gmail.com>
2021-05-12 14:00:45 -0600

    adds live list, and builds live list updater

--- react-front-end/src/contextProviders/stateContext.js
+++ react-front-end/src/contextProviders/stateContext.js
@@ -17,10 +17,18 @@ export function StateProvider(props) {
   const [state, setState] = useState(initialPins);
   const [liveList, setLiveList] = useState([]);
 
-    
- 
+  const liveListUpdate = (data) => {
+    const recentEQs = []
+    for (let i = 0; i < 5; i++) {
+      recentEQs.push(data[i])
+    }
+    return setLiveList(recentEQs);
+  };
+
+
+
   return (
-    <stateContext.Provider value={{ state, setState, liveList, setLiveList }}>
+    <stateContext.Provider value={{ state, setState, liveList, setLiveList, liveListUpdate }}>
       {props.children}
     </stateContext.Provider>
   );
