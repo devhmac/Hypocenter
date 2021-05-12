f9e35a92dd72cb32d3a0bc8ab1c3a079f04cb2a7
Devin MacGillivray <52307383+devhmac@users.noreply.github.com>
2021-05-11 18:13:44 -0600

    Revert "finalize darkmode feature"

--- react-front-end/src/components/NavBar.jsx
+++ react-front-end/src/components/NavBar.jsx
@@ -6,7 +6,6 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import ToggleBtn from "./Darkmode/ToggleBtn";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -50,7 +49,6 @@ function NavBar() {
             <Button className={classes.title}>Hypocenter</Button>
           </Typography>
           <Button className={classes.login}>Login</Button>
-          <ToggleBtn/>
         </Toolbar>
       </AppBar>
     </div>
