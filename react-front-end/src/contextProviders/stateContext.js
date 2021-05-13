df30274ec3049ee7c3f879fa6deed6d2db89d3b3
Devin MacGillivray <devhmac@gmail.com>
2021-05-13 12:04:23 -0600

    addNewLiveListItem functioning

--- react-front-end/src/contextProviders/stateContext.js
+++ react-front-end/src/contextProviders/stateContext.js
@@ -19,22 +19,34 @@ export function StateProvider(props) {
 
   const liveListUpdate = (data) => {
     const recentEQs = [];
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 6; i++) {
       recentEQs.push(data[i])
     }
     return setLiveList(recentEQs);
   };
 
-  const prependNewQuake = (data) => {
-    const recentEQs = [];
-    for (let quake of data) {
-
-    }
+  //takes in new pushed quake, and adds it to the front of the most recent quakes arr
+  const addNewLiveListItem = (data) => {
+    setLiveList(prev => {
+      const recentEQs = [...prev];
+      console.log('prev state list', recentEQs)
+      const currentEQs = [];
+      const seenEqs = {};
+      for (let quake of data) {
+        if (!seenEqs[quake.id]) {
+          currentEQs.push(quake);
+          recentEQs.pop();
+          seenEqs[quake.id] = true;
+        }
+      }
+      console.log('pushed eqs', currentEQs)
+      return [...currentEQs, ...recentEQs];
+    })
   };
 
 
   return (
-    <stateContext.Provider value={{ state, setState, liveList, setLiveList, liveListUpdate }}>
+    <stateContext.Provider value={{ state, setState, liveList, setLiveList, liveListUpdate, addNewLiveListItem }}>
       {props.children}
     </stateContext.Provider>
   );
