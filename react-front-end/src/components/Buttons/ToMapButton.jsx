eed88c80d0ad8d3c1be4c1635d53c08e8b971e06
Devin MacGillivray <devhmac@gmail.com>
2021-05-13 20:14:24 -0600

    builds to map component button

--- react-front-end/src/components/Buttons/ToMapButton.jsx
+++ react-front-end/src/components/Buttons/ToMapButton.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import Fade from "../Fade";
+import { globeLoaderContext } from "../../contextProviders/globeLoaderContext";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,17 +11,28 @@ const useStyles = makeStyles((theme) => ({
     },
     position: "absolute",
     bottom: "7%",
-    left: "3%",
+    right: "5%",
+  },
+  button: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    color: "white",
   },
 }));
 
 export default function ToMapButton(props) {
+  const { startSite } = useContext(globeLoaderContext);
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <Button variant="contained" color="primary" onClick={props.onClick}>
-        {props.children}
-      </Button>
-    </div>
+    <Fade show={startSite}>
+      <div className={classes.root}>
+        <Button
+          variant="contained"
+          className={classes.button}
+          onClick={props.onClick}
+        >
+          {props.children}
+        </Button>
+      </div>
+    </Fade>
   );
 }
