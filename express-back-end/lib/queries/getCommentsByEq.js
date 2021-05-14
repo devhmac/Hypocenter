e1783e0c26d65fd8da334fc114d9a9ed26b004f6
Devin MacGillivray <devhmac@gmail.com>
2021-05-14 17:53:38 -0600

    query working

--- express-back-end/lib/queries/getCommentsByEq.js
+++ express-back-end/lib/queries/getCommentsByEq.js
@@ -4,12 +4,12 @@ const db = require('../db');
 const getCommentsByEq = (earthquakeId) => {
 
   const queryString = `
-  SELECT users.username, comments.user_id, comments.content FROM comments
+  SELECT users.username, comments.user_id, comments.id AS comment_id, comments.content FROM comments
   JOIN users ON users.id = comments.user_id
   WHERE earthquake_id = $1`;
   const queryParams = [earthquakeId];
 
-  db.query(queryString, queryParams)
+  return db.query(queryString, queryParams)
     .then((response) => {
       return response.rows;
     });
