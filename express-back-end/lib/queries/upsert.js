8dfe99df4eef96152bb4fa2c3819de1c465810ec
Devin MacGillivray <devhmac@gmail.com>
2021-05-14 11:48:13 -0600

    now() added to upsert at $10 on insert

--- express-back-end/lib/queries/upsert.js
+++ express-back-end/lib/queries/upsert.js
@@ -12,8 +12,8 @@ const upsert = (body) => {
     }
 
     const queryString = `
-    INSERT INTO earthquakes (id, title, magnitude, pager, longitude, latitude, depth, tsunami, time_stamp)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+    INSERT INTO earthquakes (id, title, magnitude, pager, longitude, latitude, depth, tsunami, time_stamp, added)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, 10)
     ON CONFLICT (id) DO UPDATE SET title = $2, magnitude = $3, pager = $4, longitude = $5, latitude = $6, depth = $7, tsunami = $8, time_stamp = $9`;
 
     const queryParams = [
@@ -25,7 +25,8 @@ const upsert = (body) => {
       earthquake.geometry.coordinates[1],
       earthquake.geometry.coordinates[2],
       earthquake.properties.tsunami,
-      earthquake.properties.time
+      earthquake.properties.time,
+      'now()'
     ];
 
     db.query(queryString, queryParams);
