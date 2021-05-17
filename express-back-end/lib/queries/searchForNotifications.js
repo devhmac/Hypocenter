60f78ef71e42336b8e6e450b6a5edf921f03c868
David Radvan <david.radvan@gmail.com>
2021-05-17 10:38:55 -0700

    fix notifications to account for magnitude

--- express-back-end/lib/queries/searchForNotifications.js
+++ express-back-end/lib/queries/searchForNotifications.js
@@ -2,7 +2,7 @@ const { response } = require('express');
 const db = require('../db');
 
 // test query
-const searchForNotifications = (title) => {
+const searchForNotifications = (title, magnitude) => {
 
   const countryEq = title.split(',');
   const searchCountry = countryEq[1].split(" ").join('');
@@ -10,8 +10,8 @@ const searchForNotifications = (title) => {
   const queryString = `
   SELECT users.email FROM notifications
   JOIN users ON users.id = notifications.user_id
-  WHERE LOWER(country) LIKE LOWER($1)`;
-  const queryParams = ["%" + searchCountry + "%"];
+  WHERE LOWER(country) LIKE LOWER($1) AND magnitude <= $2`;
+  const queryParams = ["%" + searchCountry + "%", magnitude];
 
   return db.query(queryString, queryParams)
     .then((response) => {
